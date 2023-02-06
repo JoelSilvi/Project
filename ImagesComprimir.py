2098b588087424a10f6eae6ebda3b7d30afbebcb
Joel Aguilar <analistasilvi@gmail.com>
2023-02-05 22:16:23 -0300

    medida producto

--- ImagesComprimir.py
+++ ImagesComprimir.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 image = Image.open("C:/Project 23-01-2023/project/images/fondo_productos.jpg")
 
-quality = 10
+quality = 60
 
 image.save("C:/Project 23-01-2023/project/images/fondo_productos.jpg-lite.jpg", optimize=True, quality=quality)
 
